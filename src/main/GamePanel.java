0182d4e69b5ed0cdd1fe9751634396e7ea81d368
Sivaraj Senthilkumar <sssraj2003@gmail.com>
2022-04-04 19:40:57 -0500

    Sound in Java #9

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -22,18 +22,18 @@ public class GamePanel extends JPanel implements Runnable {
     // the world...settings
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
 
     int FPS = 60; // so that the game doesn't update a million times a second
 
+    //System
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-
-    Thread gameThread;
-
+    Sound sound = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    Thread gameThread;
+
+    //Entity and Object
     public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
 
@@ -49,6 +49,8 @@ public class GamePanel extends JPanel implements Runnable {
     public void setupGame()
     {
         aSetter.setObject();
+
+        playMusic(0);
     }
 
     public void startGameThread(){
@@ -109,5 +111,23 @@ public class GamePanel extends JPanel implements Runnable {
         player.draw(g2);
 
         g2.dispose();
+    }
+
+    public void playMusic(int i)
+    {
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+
+    public void stopMusic()
+    {
+        sound.stop();
+    }
+
+    public void playSE(int i)
+    {
+        sound.setFile(i);
+        sound.play();
     }
 }
