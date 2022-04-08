aab6d8f16fa4a2b17886b07e0f51a0f88e21b4db
Sivaraj Senthilkumar <sssraj2003@gmail.com>
2022-04-08 13:16:41 -0500

    Setup for Game Icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,31 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
-public class Main {
+public class Main extends JFrame {
     public static void main(String [] args){
 
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D RPG");
+
+        //Game Icon
+        try
+        {
+            URL resource = window.getClass().getResource("/icons/gameIcon.png");
+            BufferedImage image = ImageIO.read(resource);
+            window.setIconImage(image);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
