b71e97d6c0e448a81f5e2641988d28d33b800cc6
Sivaraj Senthilkumar <sssraj2003@gmail.com>
2022-04-12 19:49:46 -0500

    Relocated game objects to a more playable location

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -17,35 +17,35 @@ public class AssetSetter
     public void setObject()
     {
         gp.obj[0] = new OBJ_Key(gp);
-        gp.obj[0].worldX = 23 * gp.tileSize;
-        gp.obj[0].worldY = 7 * gp.tileSize;
+        gp.obj[0].worldX = 18 * gp.tileSize;
+        gp.obj[0].worldY = 17 * gp.tileSize;
 
         gp.obj[1] = new OBJ_Key(gp);
-        gp.obj[1].worldX = 23 * gp.tileSize;
-        gp.obj[1].worldY = 40 * gp.tileSize;
+        gp.obj[1].worldX = 94 * gp.tileSize;
+        gp.obj[1].worldY = 22 * gp.tileSize;
 
         gp.obj[2] = new OBJ_Key(gp);
-        gp.obj[2].worldX = 38 * gp.tileSize;
-        gp.obj[2].worldY = 8 * gp.tileSize;
+        gp.obj[2].worldX = 86 * gp.tileSize;
+        gp.obj[2].worldY = 72 * gp.tileSize;
 
         gp.obj[3] = new OBJ_Door(gp);
-        gp.obj[3].worldX = 10 * gp.tileSize;
-        gp.obj[3].worldY = 11 * gp.tileSize;
+        gp.obj[3].worldX = 55 * gp.tileSize;
+        gp.obj[3].worldY = 50 * gp.tileSize;
 
         gp.obj[4] = new OBJ_Door(gp);
-        gp.obj[4].worldX = 8 * gp.tileSize;
-        gp.obj[4].worldY = 28 * gp.tileSize;
+        gp.obj[4].worldX = 60 * gp.tileSize;
+        gp.obj[4].worldY = 50 * gp.tileSize;
 
         gp.obj[5] = new OBJ_Door(gp);
-        gp.obj[5].worldX = 12 * gp.tileSize;
-        gp.obj[5].worldY = 22 * gp.tileSize;
+        gp.obj[5].worldX = 65 * gp.tileSize;
+        gp.obj[5].worldY = 50 * gp.tileSize;
 
         gp.obj[6] = new OBJ_Chest(gp);
-        gp.obj[6].worldX = 10 * gp.tileSize;
-        gp.obj[6].worldY = 7 * gp.tileSize;
+        gp.obj[6].worldX = 88 * gp.tileSize;
+        gp.obj[6].worldY = 50 * gp.tileSize;
 
         gp.obj[7] = new OBJ_Boots(gp);
-        gp.obj[7].worldX = 37 * gp.tileSize;
-        gp.obj[7].worldY = 42 * gp.tileSize;
+        gp.obj[7].worldX = 9 * gp.tileSize;
+        gp.obj[7].worldY = 48 * gp.tileSize;
     }
 }
