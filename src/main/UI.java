13eac92811a81e2b5db84e36e21b328140864a29
aviphyr <yaqub.z.ahmed@gmail.com>
2022-04-17 21:57:46 -0500

    custom font

--- src/main/UI.java
+++ src/main/UI.java
@@ -6,13 +6,15 @@ import object.OBJ_Key;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class UI
 {
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40, arial_80B;
+    Font VCR;
     BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
@@ -25,8 +27,15 @@ public class UI
     {
         this.gp = gp;
 
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
-        arial_80B = new Font("Arial", Font.BOLD, 80);
+        try {
+            InputStream is = getClass().getResourceAsStream("/font/VCR.ttf");
+            VCR = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         //OBJ_Key key = new OBJ_Key(gp);
         //keyImage = key.image;
     }
@@ -41,7 +50,7 @@ public class UI
     {
         this.g2 = g2;
 
-        g2.setFont(arial_40);
+        g2.setFont(VCR);
         g2.setColor(Color.white);
 
         //Play State
@@ -63,7 +72,7 @@ public class UI
 
     public void drawPauseScreen()
     {
-        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 80F));
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 32F));
 
         String text = "PAUSED";
 
