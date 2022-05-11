24d3ea8decd371ca07b2f981c3ec1b10d8a9da88
Sivaraj Senthilkumar <sssraj2003@gmail.com>
2022-05-11 12:31:02 -0500

    Dash Added

--- src/cutscenes/CutSceneSetter.java
+++ src/cutscenes/CutSceneSetter.java
@@ -25,26 +25,5 @@ public class CutSceneSetter
             gp.scenes.leaveScene(3);
         }
 
-        else if (sceneNum == 1) {
-            gp.scenes.moveY(gp.player, "up", 0.5, 3, 1);
-            //gp.scenes.speech("Ah, what a lovely new home.\nRoomy, spacious, a constant flow\nof pollution through the\nroadside drains.", 2);
-            gp.scenes.moveBit("left",2);
-            gp.scenes.leaveScene(3);
-        }
-
-        else if (sceneNum == 2) {
-            gp.scenes.moveX(gp.player, "right", 0.5, 3,1);
-            //gp.scenes.speech("Hopefully more food will come\ntrough there soon.", 2);
-            gp.scenes.moveBit("up",1);
-            gp.scenes.leaveScene(2);
-        }
-
-        else if (sceneNum == 3) {
-            gp.scenes.moveY(gp.player, "down", 0.5, 3,1);
-            //gp.scenes.speech("Hopefully more food will come\ntrough there soon.", 2);
-            //gp.scenes.moveBit("left", 2);
-            gp.scenes.leaveScene(2);
-        }
-
     }
 }
